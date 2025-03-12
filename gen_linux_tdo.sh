ca294fb65978432407c2ad4900f38b2d54502401
kaz <kaz>
2025-03-12 14:32:13 -0400

    more

--- gen_linux_tdo.sh
+++ gen_linux_tdo.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
  pyinstaller --onefile main_tdo --distpath _pyinstaller/dist --workpath _pyinstaller/build
+pyinstaller --onefile main_tst.py  --distpath _pyinstaller/dist --workpath _pyinstaller/build --add-data="data_files/my_file.txt:data_files"
